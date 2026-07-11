4a45703953652f7292e68e6c00ca8bc91882fea2
Nayanthi Weerasuriya <nayanthiweerasuriya2003@gmail.com>
2026-07-11 12:30:24 +0530

    Refine portfolio navigation and floating controls

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="container footer-content">
-        <p>&copy; 2026 Nayanthi Weerasuriya</p>
+        <p>&copy; 2026 Nayanthi Weerasuriya. All rights reserved.</p>
       </div>
     </footer>
   );
